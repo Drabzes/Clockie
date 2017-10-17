90df36b78a75d2478df7f9c18feeafaebc56680f
hannecolaers <hannecolaers@hotmail.com>
2017-10-17 11:41:41 +0200

    adds alarmreceiver

--- Clockie/app/src/main/java/pxl/be/clockie/MainActivity.java
+++ Clockie/app/src/main/java/pxl/be/clockie/MainActivity.java
@@ -1,18 +1,18 @@
 package pxl.be.clockie;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.Toast;
 
-import butterknife.BindView;
-import butterknife.OnClick;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -50,6 +50,25 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, NewAlarmActivity.class);
         startActivity(intent);
         Toast.makeText(getApplicationContext(), "add workds", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onSwitchClicked(View view) {
+        Button alarmOn = (Button) view.findViewById(R.id.testButton);
+
+        alarmOn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+                Calendar calendar = Calendar.getInstance();
+
+                Intent receiverIntent = new Intent(getApplicationContext(), AlarmReceiver.class);
+                calendar.set(Calendar.HOUR_OF_DAY, 10);
+                calendar.set(Calendar.MINUTE, 50);
+                PendingIntent senderIntent = PendingIntent.getBroadcast(getApplicationContext(), 0, receiverIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+                alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), senderIntent);
+            }
+        });
     }
 
 }
